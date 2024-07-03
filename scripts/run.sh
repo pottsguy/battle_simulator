7dd40cde0d2172e5ecb0bdd3e7cc02a345751161
Guy Potts <pottsguy@gmail.com>
2024-07-03 11:38:41 +0200

    rename build.sh to package.sh; simplify run.sh

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 # -ea = Enable Assertions
 # -cp = set Class Path
-java -ea -cp ./target/db_test-1.0-SNAPSHOT-jar-with-dependencies.jar com.mycompany.app.App
+#java -ea -cp ./target/db_test-1.0-SNAPSHOT-jar-with-dependencies.jar com.mycompany.app.App
+
+mvn compile exec:java -Dexec.mainClass="com.mycompany.app.App"
